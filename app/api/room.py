712a405f3274d3ed98258ca18d30ded9b30fde6e
hxpdong <hxpdong@gmail.com>
2026-03-19 09:54:09 +0700

    update api for branch and room action

--- app/api/room.py
+++ app/api/room.py
@@ -4,7 +4,7 @@ from app.schema.room import RoomInitializeResponse, RoomListResponse, RoomTypeRe
 from app.crud import room as crud_room
 
 router = APIRouter(prefix="/admin/rooms", tags=["Admin - Rooms"])
-routerForUser = APIRouter(prefix="/user", tags=["User - Amenities"])
+routerForUser = APIRouter(prefix="/user/rooms", tags=["User - Rooms"])
 
 
 @router.post("", status_code=200)
@@ -109,6 +109,28 @@ async def rooms_list(
     Kèm tên loại phòng (JOIN room_types).
     """
     try:
-        return crud_room.get_rooms_list(branch_id=branch_id, page=page, page_size=page_size)
+        return crud_room.get_rooms_by_branch(branch_id, page, page_size, active_only=False)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Lỗi lấy danh sách phòng: {e}")
+
+@routerForUser.get("/rooms-list", response_model=RoomListResponse)
+async def user_get_active_rooms_by_branch(
+    branch_id: str, page: int = 1, page_size: int = 10
+    ):
+    return crud_room.get_rooms_by_branch(branch_id, page, page_size, active_only=True)
+
+@router.get("/{room_id}")
+async def admin_get_room_info(room_id: str):
+    """[ADMIN] Lấy thông tin chi tiết phòng (kể cả phòng đã xóa)"""
+    room = crud_room.get_room_detail(room_id, active_only=False)
+    if not room:
+        raise HTTPException(status_code=404, detail="Không tìm thấy phòng")
+    return room
+
+@routerForUser.get("/{room_id}")
+async def user_get_room_info(room_id: str):
+    """[USER] Lấy thông tin phòng (chỉ phòng đang hoạt động)"""
+    room = crud_room.get_room_detail(room_id, active_only=True)
+    if not room:
+        raise HTTPException(status_code=404, detail="Phòng không tồn tại hoặc đã ngừng hoạt động")
+    return room
